4ab065cdb2cf01f7d9302df34454572106c65ebd
kyronko <kyronko@thebreeze.co.kr>
2026-02-06 18:34:25 +0900

    서버관련 배포

---
+++ service.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    apps: [
+        {
+            name: 'opasnet4a-dev',
+            script: 'bin/www',
+            instances: 2,
+            exec_mode: 'cluster',
+            listen_timeout: 50000,
+            kill_timeout: 5000,
+            env: {
+                APPLICATION_STATUS : 'development',
+                PORT : 8080,
+                DATABASE_HOST : "211.192.7.222",
+                DATABASE_USER : "kyronko",
+                DATABASE_PASSWORD : "rnswidla1!",
+                DATABASE_PORT : 5432,
+                DATABASE_DATABASE : "opasnet_4a",
+                SESSION_SECRET : "opasnet4a_session_secret_key_2026"
+            }
+        },
+        {
+            name: 'opasnet4a-prod',
+            script: 'bin/www',
+            instances: 5,
+            exec_mode: 'cluster',
+            listen_timeout: 50000,
+            kill_timeout: 5000,
+            env: {
+                APPLICATION_STATUS : 'production',
+                PORT : 8080,
+                DATABASE_HOST : "localhost",
+                DATABASE_USER : "postgres",
+                DATABASE_PASSWORD : "password",
+                DATABASE_PORT : 5432,
+                DATABASE_DATABASE : "opasnet_4a",
+                SESSION_SECRET : "opasnet4a_session_secret_key_2026"
+            }
+        }
+    ]
+}
